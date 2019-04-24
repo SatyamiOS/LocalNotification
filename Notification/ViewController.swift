00cef463db7bbd85e9da4a9d1c245cf2daffefc5
Satyam kumar <satyamkumar10189@gmail.com>
2019-04-25 01:19:07 +0530

    Commit

---
+++ Notification/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Notification
+//
+//  Created by Satyam Kumar on 04/02/19.
+//  Copyright © 2019 Satyam Kumar. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Message"
+        content.body = "Satyam you are great!!"
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "testIdentifier", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        
+        
+    }
+
+
+}
+
